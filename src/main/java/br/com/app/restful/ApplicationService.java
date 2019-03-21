b132d74d8f5e46a1001dc2ceb142a8b5ecfa468d
Filipe Oliveira <filipe.soares@ans.gov.br>
2019-03-21 15:40:37 -0300

    WIP: Add swagger2 to docs

--- src/main/java/br/com/app/restful/ApplicationService.java
+++ src/main/java/br/com/app/restful/ApplicationService.java
@@ -1,14 +1,22 @@
 package br.com.app.restful;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import io.swagger.jaxrs.config.BeanConfig;
+import br.com.app.restful.resources.ClientResource;
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+
+// import io.swagger.jaxrs.config.BeanConfig;
 
 @ApplicationPath("/")
 public class ApplicationService extends Application {
 	
-	public ApplicationService() {
+	/*public ApplicationService() {
         BeanConfig config = new BeanConfig();
         config.setTitle("Restful Sample API");
         config.setDescription("A sample Restful api");
@@ -19,6 +27,11 @@ public class ApplicationService extends Application {
         config.setBasePath("/restful-api/v1");
         config.setResourcePackage("br.com.app.restful");
         config.setScan(true);
-    }
+    }*/
 	
+	/*@Override
+    public Set<Class<?>> getClasses() {
+        return Stream.of(ClientResource.class, OpenApiResource.class, AcceptHeaderOpenApiResource.class).collect(Collectors.toSet());
+    }
+	*/
 }
--- src/main/java/br/com/app/restful/resources/ClientResource.java
+++ src/main/java/br/com/app/restful/resources/ClientResource.java
@@ -20,37 +20,46 @@ import javax.ws.rs.core.UriBuilder;
 
 import br.com.app.restful.dao.ClientDAO;
 import br.com.app.restful.model.Client;
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+/*import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponses;*/
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @Path("/clients")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)	
-@Api(value="Clients Resource")
+// @Api(value="Clients Resource")
 public class ClientResource {
 	
 	@Inject
 	ClientDAO dao;
 
 	@GET
-	@ApiOperation(httpMethod="GET", response=Client.class, value="List all clients", responseContainer="List", code=200)
+	// @ApiOperation(httpMethod="GET", response=Client.class, value="List all clients", responseContainer="List", code=200)
+	@Operation(summary = "List all clients",  
+				description = "Retrieve a list of all clients in database", 
+				responses = {
+						@ApiResponse(responseCode = "200", content = @Content( schema = @Schema(implementation=Client.class) ) )
+						})
 	public Response list(@QueryParam("name") String name, @QueryParam("email") String email) {
 		return Response.ok(dao.list()).build();
 	}
 	
 	@GET
 	@Path("/{id}")
-	@ApiOperation(httpMethod="GET", response=Client.class, value = "Get a unique client representation")
-	@ApiResponse(code = 404, message = "Client not found")
+	/*@ApiOperation(httpMethod="GET", response=Client.class, value = "Get a unique client representation")
+	@ApiResponse(code = 404, message = "Client not found")*/
 	public Response fetch(@PathParam("id") Long id) {
 		return Response.ok(dao.find(id)).build();
 	}
 	
 	@POST
-	@ApiOperation(httpMethod="POST", response=Client.class, value = "Create a new client representation")
-	@ApiResponse(code = 409, message = "Client already exists")
+	/*@ApiOperation(httpMethod="POST", response=Client.class, value = "Create a new client representation")
+	@ApiResponse(code = 409, message = "Client already exists")*/
 	public Response create(Client client) {
 		
 		if ( dao.findByEmail(client.getEmail()).isEmpty() ){
@@ -64,8 +73,8 @@ public class ClientResource {
 	
 	@PUT
 	@Path("/{id}")
-	@ApiOperation(httpMethod="PUT", response=Client.class, value = "Update a client representation")
-	@ApiResponses( {@ApiResponse(code = 409, message = "Client already exists"), @ApiResponse(code = 404, message = "Client not found")} )
+	/*@ApiOperation(httpMethod="PUT", response=Client.class, value = "Update a client representation")
+	@ApiResponses( {@ApiResponse(code = 409, message = "Client already exists"), @ApiResponse(code = 404, message = "Client not found")} )*/
 	public Response update(@PathParam("id") Long id, Client client) {
 		
 		dao.find(id);
@@ -81,8 +90,8 @@ public class ClientResource {
 	
 	@DELETE
 	@Path("/{id}")
-	@ApiOperation(httpMethod="DELETE", response=Client.class, value = "Remove a client representation")
-	@ApiResponses(@ApiResponse(code = 404, message = "Client not found"))
+	/*@ApiOperation(httpMethod="DELETE", response=Client.class, value = "Remove a client representation")
+	@ApiResponses(@ApiResponse(code = 404, message = "Client not found"))*/
 	public Response remover(@PathParam("id") Long id) {
 		dao.remove(dao.find(id));
 		return Response.noContent().build();
